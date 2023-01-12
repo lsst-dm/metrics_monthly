a6f1ee417cc6e6e8958ccbff2a0e22da8a1d9c6c
Orion Eiger <quasardragon96@gmail.com>
2023-01-12 15:29:44 -0800

    Update for w_2023_01

--- HSC-RC2/dispatch/dispatch.sh
+++ HSC-RC2/dispatch/dispatch.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 # Look up at https://eups.lsst.codes/stack/src/tags/
-export DATETAG="2022-10-27T08:20"
+export DATETAG="2022-11-24T08:04"
 
 
 export DATASET="HSC_RC2"
@@ -10,9 +10,9 @@ export RUN_ID="DM-37089"
 export RUN_ID_URL="https://jira.lsstcorp.org/browse/DM-37089"
 export VERSION_TAG="w_2022_48"
 
-export SQUASH_USER=
-export SQUASH_password=
+export SQUASH_USER=verification
+export SQUASH_password=2DfjXgWBL964NVDFRby3
 
-export JSON_FOLDER=/fs/ddn/sdf/group/rubin/sandbox/hchiang2/metrics_json/w_2022_48/
+export JSON_FOLDER=/fs/ddn/sdf/group/rubin/sandbox/eiger/metrics_json/w_2022_48/
 ls -A1 $JSON_FOLDER | awk -v DATETAG="$DATETAG" '{print "dispatch_verify.py $JSON_FOLDER/" $1,"--ignore-blobs --ignore-lsstsw  --url https://squash-restful-api.lsst.codes --env=ldf --date-created "DATETAG":00Z"}' | bash
 
--- HSC-RC2/dispatch/make_json.sl
+++ HSC-RC2/dispatch/make_json.sl
@@ -6,9 +6,9 @@
 #SBATCH --mem=20G
 #SBATCH -t 24:00:00
 #SBATCH -J verify
-#SBATCH --output=/fs/ddn/sdf/group/rubin/sandbox/hchiang2/metrics_json/make_json_w_2022_48.%j.log
-#SBATCH --error=/fs/ddn/sdf/group/rubin/sandbox/hchiang2/metrics_json/make_json_w_2022_48.%j.log
-#SBATCH --chdir /fs/ddn/sdf/group/rubin/sandbox/hchiang2/metrics_json/w_2022_48
+#SBATCH --output=/fs/ddn/sdf/group/rubin/sandbox/eiger/metrics_json/make_json_w_2022_48.%j.log
+#SBATCH --error=/fs/ddn/sdf/group/rubin/sandbox/eiger/metrics_json/make_json_w_2022_48.%j.log
+#SBATCH --chdir /fs/ddn/sdf/group/rubin/sandbox/eiger/metrics_json/w_2022_48
 
 srun gen3_to_job.py /sdf/group/rubin/repo/main_20210215/  "HSC/runs/RC2/w_2022_48/DM-37089" --dataset_name 'HSC_RC2'
 
