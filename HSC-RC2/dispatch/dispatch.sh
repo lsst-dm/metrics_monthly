bea211eb758e8369437611cdcf11d402987bccda
Hsin-Fang Chiang <hchiang@lsst.org>
2023-02-14 09:19:53 -0800

    Update dispatch scripts for w_2023_03

--- HSC-RC2/dispatch/dispatch.sh
+++ HSC-RC2/dispatch/dispatch.sh
@@ -1,15 +1,15 @@
 #!/bin/env bash
 
 # Look up at https://eups.lsst.codes/stack/src/tags/
-export DATETAG="2023-01-05T09:48"
+export DATETAG="2023-01-19T09:24"
 
 
 export DATASET="HSC_RC2"
 export DATASET_REPO_URL="https://community.lsst.org/t/shared-gen3-data-repositories-ready-for-some-use/4845"
-export RUN_ID="DM-37483"
-export RUN_ID_URL="https://jira.lsstcorp.org/browse/DM-37483"
-export VERSION_TAG="w_2023_01"
+export RUN_ID="DM-37570"
+export RUN_ID_URL="https://jira.lsstcorp.org/browse/DM-37570"
+export VERSION_TAG="w_2023_03"
 
-export JSON_FOLDER=/fs/ddn/sdf/group/rubin/sandbox/hchiang2/metrics_json/w_2023_01/
+export JSON_FOLDER=/fs/ddn/sdf/group/rubin/sandbox/hchiang2/metrics_json/w_2023_03/
 ls -A1 $JSON_FOLDER | awk -v DATETAG="$DATETAG" '{print "dispatch_verify.py $JSON_FOLDER/" $1,"--ignore-blobs --ignore-lsstsw  --url https://squash-restful-api.lsst.codes --env=ldf --date-created "DATETAG":00Z"}' | bash
 
--- HSC-RC2/dispatch/make_json.sl
+++ HSC-RC2/dispatch/make_json.sl
@@ -6,9 +6,9 @@
 #SBATCH --mem=20G
 #SBATCH -t 24:00:00
 #SBATCH -J verify
-#SBATCH --output=/fs/ddn/sdf/group/rubin/sandbox/hchiang2/metrics_json/make_json_w_2023_01.%j.log
-#SBATCH --error=/fs/ddn/sdf/group/rubin/sandbox/hchiang2/metrics_json/make_json_w_2023_01.%j.log
-#SBATCH --chdir /fs/ddn/sdf/group/rubin/sandbox/hchiang2/metrics_json/w_2023_01
+#SBATCH --output=/fs/ddn/sdf/group/rubin/sandbox/hchiang2/metrics_json/make_json_w_2023_03.%j.log
+#SBATCH --error=/fs/ddn/sdf/group/rubin/sandbox/hchiang2/metrics_json/make_json_w_2023_03.%j.log
+#SBATCH --chdir /fs/ddn/sdf/group/rubin/sandbox/hchiang2/metrics_json/w_2023_03
 
-srun gen3_to_job.py /sdf/group/rubin/repo/main_20210215/  "HSC/runs/RC2/w_2023_01/DM-37483" --dataset_name 'HSC_RC2'
+srun gen3_to_job.py /sdf/group/rubin/repo/main_20210215/  "HSC/runs/RC2/w_2023_03/DM-37570" --dataset_name 'HSC_RC2'
 
