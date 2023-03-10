54d35cd21d03373f9ef5c7263231513ff8489645
Orion Eiger <quasardragon96@gmail.com>
2023-03-10 15:15:06 -0800

    Check PanDA authentication on setup

--- HSC-RC2/setup_env.sh
+++ HSC-RC2/setup_env.sh
@@ -26,3 +26,6 @@ export https_proxy=http://atlsquid.slac.stanford.edu:3128
 export http_proxy=http://atlsquid.slac.stanford.edu:3128
 export HTTPS_PROXY=http://atlsquid.slac.stanford.edu:3128
 export SQUID_PROXY=http://atlsquid.slac.stanford.edu:3128
+
+# Check PanDA authentication
+panda_auth status
